0c03c5a238a71b142a358760dd601ea14295092b
xw <92379590+MyCode12-maker@users.noreply.github.com>
2025-08-08 14:59:26 +0800

    Add files via upload

---
+++ ETT_script/FPS_ETTh2.sh
@@ -0,0 +1,84 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=FPS #DLinear
+seq_len=96
+batch_size=256
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len_96 \
+  --model $model_name \
+  --data ETTh2 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len 96 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.01 \
+  --batch_size $batch_size \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len_192 \
+  --model $model_name \
+  --data ETTh2 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len 192 \
+  --learning_rate 0.01 \
+  --batch_size $batch_size \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len_336 \
+  --model $model_name \
+  --data ETTh2 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len 336 \
+  --learning_rate 0.01 \
+  --batch_size $batch_size \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTh2.csv \
+  --model_id ETTh2_$seq_len_720 \
+  --model $model_name \
+  --data ETTh2 \
+  --features M \
+  --seq_len $seq_len \
+  --label_len 48 \
+  --pred_len 720 \
+  --learning_rate 0.01 \
+  --batch_size $batch_size \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
---
+++ ETT_script/FPS_ETTm2.sh
@@ -0,0 +1,83 @@
+export CUDA_VISIBLE_DEVICES=0
+
+model_name=FPS #DLinear
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_96_96 \
+  --model $model_name \
+  --data ETTm2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 96 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --learning_rate 0.01 \
+  --batch_size 256 \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_96_192 \
+  --model $model_name \
+  --data ETTm2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 192 \
+  --learning_rate 0.01 \
+  --batch_size 256 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_96_336 \
+  --model $model_name \
+  --data ETTm2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --learning_rate 0.01 \
+  --batch_size 256 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
+
+python -u run.py \
+  --task_name long_term_forecast \
+  --is_training 1 \
+  --root_path ./dataset/ETT-small/ \
+  --data_path ETTm2.csv \
+  --model_id ETTm2_96_720 \
+  --model $model_name \
+  --data ETTm2 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --learning_rate 0.01 \
+  --batch_size 256 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
